10c207de091f131e6b0365ed3b6df9ae7792c6ba
Jose Rubio <ru310z@gmail.com>
2021-07-02 21:06:59 -0500

    Task data now shows on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,8 +10,22 @@ function getTasks(){
         method: 'GET',
         url: '/task'
     }).then(response => {
-        console.log('List of tasks:', response);
+        showTasks(response);
     }).catch(response => {
         console.log('Error getting tasks', response);
     })
+}
+
+function showTasks(tasks){
+    $('#tasksTable').empty();
+    for (const task of tasks) {
+        $('#tasksTable').append(`
+            <tr>
+                <th>${task.task}</th>
+                <th>${task.isComplete}</th>
+                <th><button>Complete</button></th>
+                <th><button>Delete</button></th>
+            </tr>
+        `)
+    }
 }
